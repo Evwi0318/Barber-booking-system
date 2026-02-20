0e063feb7b1cc1e31124c57aca24c2f63b7115e2
Evan <wilbardevan03@gmail.com>
2026-02-21 00:27:47 +0100

    Chore(db): add ef core, db context and initial migration

--- src/BarberBooking.api/Program.cs
+++ src/BarberBooking.api/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using BarberBooking.api.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddProblemDetails();
 builder.Services.AddHealthChecks();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
