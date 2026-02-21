7a65659806500f33887c40c469cd84e6a7a8441a
Evan Wilbard <163440658+Evwi0318@users.noreply.github.com>
2026-02-21 22:21:41 +0100

    feat(services): add service entity and db migrations (#4)

---
+++ src/BarberBooking.api/Features/Services/Service.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberBooking.api.Features.Services;
+
+public class Service
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Name { get; set; } = null!;
+
+    public int DurationMinutes { get; set; }
+
+    public decimal Price { get; set; }
+
+    public bool IsActive { get; set; } = true;
+}
--- src/BarberBooking.api/Infrastructure/Persistence/AppDbContext.cs
+++ src/BarberBooking.api/Infrastructure/Persistence/AppDbContext.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using BarberBooking.api.Features.Services;
+
 
 
 namespace BarberBooking.api.Infrastructure.Persistence;
@@ -10,4 +12,6 @@ namespace BarberBooking.api.Infrastructure.Persistence;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Service> Services => Set<Service>();
 }
